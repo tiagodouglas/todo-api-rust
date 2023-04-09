2c7d01e530844afed4bc610397dcc63702e6323e
Tiago Douglas <tiagodouglas.c@gmail.com>
2023-04-09 16:09:52 -0300

    fix: User filter

--- application/src/auth/authenticate.rs
+++ application/src/auth/authenticate.rs
@@ -1,8 +1,8 @@
 use super::jwt::generate_token;
-use bcrypt::{hash, verify, DEFAULT_COST};
+use bcrypt::{verify};
 use diesel::prelude::*;
 use domain::{
-    schema::users::{self, email},
+    schema::users::{self},
     user::User,
 };
 use infrastructure::establish_connection;
@@ -11,7 +11,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize)]
 pub struct AuthResponse {
     token: String,
-    exp: usize
+    exp: usize,
 }
 
 #[derive(Deserialize)]
@@ -29,27 +29,19 @@ pub struct AuthError {
 pub fn authenticate_user(auth_request: AuthRequest) -> Result<AuthResponse, AuthError> {
     let result = std::panic::catch_unwind(|| {
         let user = match users::table
-            .filter(users::email.eq(email))
+            .filter(users::email.eq(&auth_request.email))
             .first::<User>(&mut establish_connection())
         {
-            Ok(u) => Some(u),
-            Err(diesel::NotFound) => None,
-            Err(err) => {
+            Ok(u) => u,
+            Err(diesel::NotFound) => {
                 return Err(AuthError {
-                    message: format!("Database error: {}", err),
-                    status: 500,
+                    message: "User not found".to_owned(),
+                    status: 404,
                 })
             }
+            Err(err) => panic!("Database error: {}", err),
         };
 
-        if user.is_none() {
-            return Err(AuthError {
-                message: "User not found".to_owned(),
-                status: 400,
-            });
-        }
-
-        let user = user.unwrap();
         let hashed_password = &user.hash;
 
         let password_matches = verify(auth_request.password, hashed_password).unwrap();
@@ -65,7 +57,10 @@ pub fn authenticate_user(auth_request: AuthRequest) -> Result<AuthResponse, Auth
 
         let token = generate_token(&sub).unwrap();
 
-        return Ok(AuthResponse { token: token.token, exp: token.exp });
+        return Ok(AuthResponse {
+            token: token.token,
+            exp: token.exp,
+        });
     });
 
     match result {
