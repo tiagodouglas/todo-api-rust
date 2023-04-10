862e5db0a5bf4a0a22fcacf0045126c25d25e07e
Tiago Douglas <tiagodouglas.c@gmail.com>
2023-04-09 21:17:20 -0300

    feat: Create todo migration

--- domain/src/schema.rs
+++ domain/src/schema.rs
@@ -1,6 +1,17 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    todos (id) {
+        id -> Int4,
+        description -> Varchar,
+        completed -> Bool,
+        userid -> Int4,
+        datecreated -> Timestamptz,
+        dateupdated -> Nullable<Timestamptz>,
+    }
+}
+
+diesel::table! {
     users (id) {
         id -> Int4,
         email -> Varchar,
@@ -9,3 +20,10 @@ diesel::table! {
         dateupdated -> Nullable<Timestamptz>,
     }
 }
+
+diesel::joinable!(todos -> users (userid));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    todos,
+    users,
+);
